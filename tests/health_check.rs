99479c59413d6531b01f90f8ef59239b56f02c79
kostant <kostant.stanton@gmail.com>
2023-04-29 14:10:09 -0400

    add rust fmt on save to vscode
    now using rust fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,7 +2,8 @@ use std::net::TcpListener;
 use zero2prod::run;
 
 fn spawn_app() -> String {
-    let listener: TcpListener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let listener: TcpListener =
+        TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let server = run(listener).expect("Failed to bind address");
