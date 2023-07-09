cf8c21f6bb792afb0243ca7ff944ac5339dc1cfb
kostant <kostant.stanton@gmail.com>
2023-07-09 14:52:32 -0400

    adds docker command

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -24,13 +24,13 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 # Launch postgres using Docker
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
+docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
 
 # Sometimes the Docker DB isn't ready to accept commands
